1d845c2d3a781005920dd58e06d56c59568cb732
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-04 03:06:45 +0100

    Add project files.

---
+++ Goto_in_Csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int i = 1;
+        System.Console.WriteLine("Sola");
+        System.Console.WriteLine("Tayo");
+        System.Console.WriteLine("Dare");
+        mylabel:
+        System.Console.WriteLine("Seun");
+        System.Console.WriteLine("Funke");
+        System.Console.WriteLine("Segun");
+        i++;
+        if(i <= 5)
+        {
+            goto mylabel;
+        }
+        System.Console.WriteLine("Kunle");
+        System.Console.WriteLine("Ayinke");
+
+        System.Console.ReadKey();
+    }
+}
